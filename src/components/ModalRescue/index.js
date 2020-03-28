20ef332e31135c504372d469623ab1e264616f3e
alanrslima <alanronison.lima@gmail.com>
2020-03-28 14:18:27 -0300

    Add proptypes at modalRescue

--- src/components/ModalRescue/index.js
+++ src/components/ModalRescue/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Modal from 'react-native-modal';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 import Button from '../Button';
 
-export default function ModalRescue({ isVisible, onCancel }) {
+function ModalRescue({ isVisible, onCancel }) {
   return (
     <Modal
       backdropOpacity={0.3}
@@ -26,3 +27,10 @@ export default function ModalRescue({ isVisible, onCancel }) {
     </Modal>
   );
 }
+
+ModalRescue.propTypes = {
+  isVisible: PropTypes.bool.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default ModalRescue;
